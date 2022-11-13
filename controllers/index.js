8cdb717626e9dcb4fb6c3a164fb13c27a48fc807
Sandeep Kumar Shah <sandeep2016shah@gmail.com>
2022-11-14 01:10:04 +0530

    added route and action to post art

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
-const Art = require('../model/art');
-const Users = require('../model/user');
-const dotenv = require('dotenv');
+const Art = require("../model/art");
+const Users = require("../model/user");
+const dotenv = require("dotenv");
 dotenv.config();
-const { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const bucketName = process.env.BUCKET_NAME;
 const region = process.env.BUCKET_REGION;
@@ -12,82 +17,133 @@ const accessKeyId = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
 const s3 = new S3Client({
-    credentials: {
-        accessKeyId,
-        secretAccessKey,
-    },
-    region,
-})
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+  region,
+});
 
 module.exports.createUser = async (req, res) => {
-    try {
-        const { 
-            email = '', 
-            name = '', 
-            phoneNo, 
-            bio = '', 
-            address = '', 
-        } = req.body || {};
-        const { originalname: profilePic = '', 
-                buffer = '', 
-                mimetype = '' 
-            } =  req.file || {};
-        const existingUser = await Users.findOne({ email });
-        if(existingUser) {
-            return res.status(200).json({
-                message: "User already exist",
-            })
-        }
-        // need to add jwt token
-        const putParams = {
-            Bucket: bucketName,
-            Key: profilePic,
-            Body: buffer,
-            ContentType: mimetype,
-        };
-
-        const putCommand = new PutObjectCommand(putParams);
-        await s3.send(putCommand);
-
-        const newUser = await Users.create({
-            email,
-            name,
-            phoneNo, 
-            bio, 
-            profilePic,
-            address,
-        });
-
-        const getParams = {
-            Bucket: bucketName,
-            Key: profilePic,
-        }
-
-        const getCommand = new GetObjectCommand(getParams);
-        const imageUrl = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
-
-        // try to add imageUrl data in ProfilePic of user object
-        console.log("newUser", newUser);
-        const token = jwt.sign({id: newUser._id, email: newUser.email}, process.env.JWT_SECRET, { expiresIn: "10000000" });
-        console.log("token", token);
-        return res.status(200).json({
-            message: 'user is added successfully!!!',
-            status: 'success',
-            user: newUser,
-            profilePic: imageUrl,
-            token,
-        })
-    } catch(error) {
-        return res.status(500).json({
-            message: "Failed to add user!!!",
-            status: 'failed',
-        })
+  try {
+    const {
+      email = "",
+      name = "",
+      phoneNo,
+      bio = "",
+      address = "",
+    } = req.body || {};
+    const {
+      originalname: profilePic = "",
+      buffer = "",
+      mimetype = "",
+    } = req.file || {};
+    const existingUser = await Users.findOne({ email });
+    if (existingUser) {
+      return res.status(200).json({
+        message: "User already exist",
+      });
     }
+
+    const putParams = {
+      Bucket: bucketName,
+      Key: profilePic,
+      Body: buffer,
+      ContentType: mimetype,
+    };
+
+    const putCommand = new PutObjectCommand(putParams);
+    await s3.send(putCommand);
+
+    const getParams = {
+        Bucket: bucketName,
+        Key: profilePic,
+      };
+
+    const getCommand = new GetObjectCommand(getParams);
+    const imageUrl = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
+
+    const newUser = await Users.create({
+      email,
+      name,
+      phoneNo,
+      bio,
+      profilePic: imageUrl,
+      address,
+    });
+
+    const token = jwt.sign(
+      { id: newUser._id, email: newUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "10000000" }
+    );
+
+    return res.status(200).json({
+      message: "user is added successfully!!!",
+      status: "success",
+      user: newUser,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to add user!!!",
+      status: "failed",
+    });
+  }
 };
 
 module.exports.test = async (req, res) => {
-    return res.status(200).json({
-        message:"tested"
-    })
-}
+  return res.status(200).json({
+    message: "tested",
+  });
+};
 
+module.exports.createArt = async (req, res) => {
+  try {
+    const { title, description, price, totalPiece, userId } = req.body || {};
+    const pictures = [];
+    for (let picture of req.files) {
+      const { originalname = "", buffer = "", mimetype = "" } = picture || {};
+
+      const putParams = {
+        Bucket: bucketName,
+        Key: originalname,
+        Body: buffer,
+        ContentType: mimetype,
+      };
+
+      const putCommand = new PutObjectCommand(putParams);
+      await s3.send(putCommand);
+
+      const getParams = {
+        Bucket: bucketName,
+        Key: originalname,
+      };
+      
+      const getCommand = new GetObjectCommand(getParams);
+      const imageUrl = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
+      pictures.push(imageUrl);
+    }
+
+    const art = await Art.create({
+      title,
+      description,
+      price,
+      totalPiece,
+      user: userId,
+      pictures,
+    });
+
+    return res.status(200).json({
+      message: "successfully created the product",
+      addedProduct: art,
+      status: "success",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "error while creating product!",
+      error,
+      status: "failure",
+    });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,28 @@
-const router = require('express').Router();
-const passport = require('../config/passportJWT');
+const router = require("express").Router();
+const passport = require("../config/passportJWT");
 
-const multer = require('multer');
+const multer = require("multer");
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-const userControllers = require('../controllers/index');
+const userControllers = require("../controllers/index");
 
-router.post('/add-user',upload.single('image'), userControllers.createUser);
+router.post("/add-user", upload.single("image"), userControllers.createUser);
 
-router.get('/test', passport.authenticate('jwt', {session: false}), userControllers.test);
+router.post(
+  "/add-art",
+  upload.array('photos', 12),
+  userControllers.createArt
+);
 
-router.get('/', (req,res) => {
-    return res.send('hello');
+router.get(
+  "/test",
+  passport.authenticate("jwt", { session: false }),
+  userControllers.test
+);
+
+router.get("/", (req, res) => {
+  return res.send("hello");
 });
 
 module.exports = router;
